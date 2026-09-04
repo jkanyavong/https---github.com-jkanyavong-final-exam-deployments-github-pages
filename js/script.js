59e5de573f5a2558709c2e81f436f0a4e11a41cb
Johnny Kanyavong <jkanyavong@student.scitech.edu>
2026-09-03 19:38:45 -0700

    Complete final exam project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const registerButton = document.getElementById('register-btn');
+
+// Listen for a "click" event on the button
+changeButton.addEventListener('click', function(event) {
+    // Code inside here will run ONLY when the button is clicked
+    // alert('Button was clicked!');
+})
